a7d30f4faad5f915091032647242d848e7ad01d6
sj56168 <513674168@qq.com>
2019-05-29 00:38:17 +0800

    Initial commit

---
+++ src/java/com/jason/ActiveMQUtil.java
@@ -0,0 +1,20 @@
+package com.jason;
+
+import cn.hutool.core.util.NetUtil;
+
+import javax.swing.*;
+
+/**
+ * Created by 我不管我最帅 on 2019/5/28.
+ */
+public class ActiveMQUtil {
+    public static void main(String[] args) {
+        checkServer();
+    }
+    public static void checkServer() {
+        if(NetUtil.isUsableLocalPort(8161)) {
+            JOptionPane.showMessageDialog(null, "ActiveMQ 服务器未启动 ");
+            System.exit(1);
+        }
+    }
+}
